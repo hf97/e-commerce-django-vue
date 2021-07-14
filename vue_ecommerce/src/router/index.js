06e857faab18c7fa3ba22009b5beb516efd71325
HF97 <hugoanf97@gmail.com>
2021-07-14 19:13:40 +0100

    Sign up.

--- vue_ecommerce/src/router/index.js
+++ vue_ecommerce/src/router/index.js
@@ -4,6 +4,8 @@ import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
@@ -38,6 +40,16 @@ const routes = [
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/sign-up',
+    name: 'Sign-up',
+    component: SignUp
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
 ]
 
