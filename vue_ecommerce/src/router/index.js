397f8e79afb46f9f42729f0214aae86ed2e698c0
HF97 <hugoanf97@gmail.com>
2021-07-15 15:17:35 +0100

    CHeckout

--- vue_ecommerce/src/router/index.js
+++ vue_ecommerce/src/router/index.js
@@ -8,6 +8,7 @@ import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/Login.vue'
 import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
 
 const routes = [
   {
@@ -57,6 +58,14 @@ const routes = [
     path: '/my-account',
     name: 'MyAccount',
     component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
     meta: {
       requireLogin: true
     }
